aca0641db26cba89d0841a36646a463b22cda6f7
bohndezdev <bohndez.dev@gmail.com>
2020-08-09 20:30:53 -0400

    Add | Checkbox validator.

--- src/validators/validation-types/index.js
+++ src/validators/validation-types/index.js
@@ -1,3 +1,4 @@
 export { requiredField } from './requiredField.validator'
 export { textField } from './textField.validator'
 export { emailField } from './emailField.validator'
+export { checkboxField } from './checkboxField.validator'
