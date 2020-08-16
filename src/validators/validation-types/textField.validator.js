4f2a22270c56257cee5de715ea51cac684180f3e
bohndezdev <bohndez.dev@gmail.com>
2020-08-16 15:44:55 -0400

    Fix | textField.validator.js: Fix validate text with spaces

--- src/validators/validation-types/textField.validator.js
+++ src/validators/validation-types/textField.validator.js
@@ -4,7 +4,7 @@
  * @return {boolean} Return true if has only letters and false if not.
  */
 export const textField = (field) => {
-  const expr = /^[a-zA-Z]*$/
+  const expr = /^[a-zA-Z\s]*$/
   if (!expr.test(field.value)) {
     return false
   } else {
